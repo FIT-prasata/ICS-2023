dd1d42187672f5a3609215a92df652151f0eb2de
Marek Prochazka <m.prochazka2002@gmail.com>
2023-03-04 08:36:15 +0100

    #5: (initial migrations)

--- TimeTracker.DAL/Factories/DesignTimeDbContextFactory.cs
+++ TimeTracker.DAL/Factories/DesignTimeDbContextFactory.cs
@@ -21,7 +21,7 @@ public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<TimeTracke
         //         Encrypt = False;
         //         TrustServerCertificate = True;");
 
-        builder.UseSqlite($"Data Source=TimeTracker;Cache=Shared");
+        builder.UseSqlite($"Data Source=TimeTrackerDB;Cache=Shared");
 
         return new TimeTrackerDbContext(builder.Options);
     }
