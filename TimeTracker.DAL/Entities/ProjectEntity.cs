ef4fc41a631bab8bf088cd91604bd6e2d6268e9c
Marek Prochazka <m.prochazka2002@gmail.com>
2023-03-04 08:16:15 +0100

    #5: (entity related changes)

--- TimeTracker.DAL/Entities/ProjectEntity.cs
+++ TimeTracker.DAL/Entities/ProjectEntity.cs
@@ -4,6 +4,12 @@ public class ProjectEntity : IEntity
 {
     public required string Name { get; set; }
 
-    public List<ActivityEntity> Activities { get; set; }
-    public List<ProjectUserEntity> Users { get; set; }
+    public string? Description { get; set; }
+
+    public required Guid CreatedById { get; set; }
+
+    public UserEntity? CreatedBy { get; set; }
+
+    public List<ActivityEntity>? Activities { get; set; }
+    public List<ProjectUserEntity>? Users { get; set; }
 }
--- TimeTracker.DAL/Entities/UserEntity.cs
+++ TimeTracker.DAL/Entities/UserEntity.cs
@@ -6,10 +6,10 @@
         public required string LastName { get; set; }
         public string? ImgUri { get; set; }
 
-        public List<ActivityEntity> Activities { get; set; }
+        public List<ActivityEntity>? Activities { get; set; }
         
-        public List<ActivityEntity> AuthoredActivities { get; set; }
-        public List<ProjectUserEntity> Projects { get; set; }
+        public List<ActivityEntity>? AuthoredActivities { get; set; }
+        public List<ProjectUserEntity>? Projects { get; set; }
 
 
 
