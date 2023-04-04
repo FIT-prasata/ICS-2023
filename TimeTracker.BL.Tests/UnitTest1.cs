f44473e0e0b538d4c85c6afb2113a8c8a099e553
David Sklenar <dav.sklen@gmail.com>
2023-04-04 20:24:20 +0200

    Created BL and BL.Tests projects

---
+++ TimeTracker.BL.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TimeTracker.BL.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TimeTracker.BL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TimeTracker.BL
+{
+    public class Class1
+    {
+
+    }
+}
