617f378592ef0607849454c6fa13eb63681fbd19
David Sklenar <dav.sklen@gmail.com>
2023-04-04 19:05:26 +0200

    Mappers (probably) done

---
+++ TimeTracker.DAL/Mappers/UserEntityMapper.cs
@@ -0,0 +1,13 @@
+﻿using TimeTracker.DAL.Entities;
+
+namespace TimeTracker.DAL.Mappers;
+
+public class UserEntityMapper : IEntityMapper<UserEntity>
+{
+    public void MapToExistingEntity(UserEntity existingEntity, UserEntity newEntity)
+    {
+        existingEntity.FirstName = newEntity.FirstName;
+        existingEntity.LastName = newEntity.LastName;
+        existingEntity.ImgUri = newEntity.ImgUri;
+    }
+}
