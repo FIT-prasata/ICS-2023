4410625f52bcc6dcaf112cd93c843af9531585d7
Adam Světlík <adam.m.svetlik@gmail.com>
2023-04-05 20:31:20 +0200

    user tests

--- TimeTracker.DAL.Tests/UserTests.cs
+++ TimeTracker.DAL.Tests/UserTests.cs
@@ -31,12 +31,89 @@ namespace TimeTracker.DAL.Tests
             Assert.Equal((entity.Id, entity.FirstName, entity.LastName, entity.ImgUri), (actualEntities.Id, actualEntities.FirstName, actualEntities.LastName, actualEntities.ImgUri));
         }
 
-
         [Fact]
-        public void SuccessfullTest()
+        public async Task AddNew_User_With_Existing_Id()
         {
-            Assert.True(true);
+            //Arrange
+            UserEntity entity = new()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Igor",
+                LastName = "Rogi",
+                ImgUri = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+            };
+
+            UserEntity entity2 = new()
+            {
+                Id = entity.Id,
+                FirstName = "Igor",
+                LastName = "Rogi",
+                ImgUri = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+            };
+            //Act
+            TimeTrackerDbContextSUT.Users.Add(entity);
+            await TimeTrackerDbContextSUT.SaveChangesAsync();
+
+            try
+            {
+                TimeTrackerDbContextSUT.Users.Add(entity2);
+                await TimeTrackerDbContextSUT.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            Assert.True(false);
         }
 
+        [Fact]
+        public async Task DeleteExisting_User()
+        {
+            //Arrange
+            UserEntity entity = new()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Igor",
+                LastName = "Rogi",
+                ImgUri = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+            };
+            //Act
+            TimeTrackerDbContextSUT.Users.Add(entity);
+            await TimeTrackerDbContextSUT.SaveChangesAsync();
+            TimeTrackerDbContextSUT.Users.Remove(entity);
+            await TimeTrackerDbContextSUT.SaveChangesAsync();
+            //Assert
+            await using var dbx = await DbContextFactory.CreateDbContextAsync();
+            var actualEntities = await dbx.Users.SingleOrDefaultAsync(i => i.Id == entity.Id);
+            Assert.Null(actualEntities);
+        }
+
+        [Fact]
+        public async Task DeleteNonExisting_User()
+        {
+            //Arrange
+            UserEntity entity = new()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Igor",
+                LastName = "Rogi",
+                ImgUri = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+            };
+            //Act
+            try
+            {
+                TimeTrackerDbContextSUT.Users.Remove(entity);
+                await TimeTrackerDbContextSUT.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                Assert.True(false);
+                return;
+            }
+            //Assert
+            await using var dbx = await DbContextFactory.CreateDbContextAsync();
+            var actualEntities = await dbx.Users.SingleOrDefaultAsync(i => i.Id == entity.Id);
+            Assert.Null(actualEntities);
+        }
     }
 }
