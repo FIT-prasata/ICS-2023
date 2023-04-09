e685cfb8c034cfffe3fdabfbb71c373669893c36
Adam Světlík <adam.m.svetlik@gmail.com>
2023-04-09 21:59:13 +0200

    project facade tests file init
    (cherry picked from commit c99f62e9cf29ef70252a33ed2e7a1937cbcb3b41)

---
+++ TimeTracker.BL.Tests/FacadesTests/ProjectFacadeTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.DAL.UnitOfWork;
+using Xunit.Abstractions;
+
+namespace TimeTracker.BL.Tests.FacadesTests
+{
+    internal class ProjectFacadeTests : FacadeTestsBase
+    {
+
+        public ProjectFacadeTests(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
