d7573f1eaab4c503e424aaf3adc80db7b0bb542e
David Sklenar <dav.sklen@gmail.com>
2023-04-09 20:33:50 +0200

    facade xd

---
+++ TimeTracker.BL.Tests/FacadesTests/UserFacadeTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace TimeTracker.BL.Tests.FacadesTests
+{
+    internal class UserFacadeTests
+    {
+        public UserFacadeTests(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
