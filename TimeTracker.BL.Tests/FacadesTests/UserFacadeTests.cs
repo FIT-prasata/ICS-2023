37bb24a429cf6117e0607832771336ca3d2b2495
Matěj Skopalík <xskopa17@vutbr.cz>
2023-04-09 21:31:14 +0200

    fix typo, add simple test for user facade

--- TimeTracker.BL.Tests/FacadesTests/UserFacadeTests.cs
+++ TimeTracker.BL.Tests/FacadesTests/UserFacadeTests.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeTracker.BL.Facades;
+using TimeTracker.BL.Models;
+using TimeTracker.Common.Tests.Seeds;
+using TimeTracker.DAL.UnitOfWork;
 using Xunit.Abstractions;
+
 
 namespace TimeTracker.BL.Tests.FacadesTests
 {
@@ -11,6 +16,17 @@ namespace TimeTracker.BL.Tests.FacadesTests
     {
         public UserFacadeTests(ITestOutputHelper output) : base(output)
         {
+        }
+        [Fact]
+        public async Task UserFacadeTestGet()
+        {
+            var facade = new UserFacade(new Mappers.UserModelMapper(), UnitOfWorkFactory);
+            var userEnum = await facade.GetAsync();
+            Assert.NotNull(userEnum);
+            var fmodel = userEnum.Where(i => i.Id == UserSeeds.UserGet.Id).ToList().First();
+            UserListModel emodel = UserModelMapper.MapToListModel(UserSeeds.UserGet);
+            Assert.NotNull(emodel);
+            Assert.Equal(fmodel,emodel);
         }
     }
 }
