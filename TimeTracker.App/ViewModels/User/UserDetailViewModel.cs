4c5abb47e22fbdbd855b4fce791da415c724fd32
Marek Prochazka <m.prochazka2002@gmail.com>
2023-05-14 11:57:50 +0200

    build?

--- TimeTracker.App/ViewModels/User/UserDetailViewModel.cs
+++ TimeTracker.App/ViewModels/User/UserDetailViewModel.cs
@@ -9,16 +9,14 @@ using TimeTracker.BL.Models;
 
 namespace TimeTracker.App.ViewModels.User;
 
-[QueryProperty(nameof(UserId), nameof(UserId))]
 public partial class UserDetailViewModel : ViewModelBase
 {
     private readonly UserFacade _userFacade;
     private readonly INavigationService _navigationService;
     private readonly IAlertService _alertService;
 
-    public Guid UserId { get; set; }
 
-    public UserDetailModel? User { get; private set; }
+    public IEnumerable<UserListModel> Users { get; private set; }
 
     public UserDetailViewModel(
             UserFacade userFacade,
@@ -37,7 +35,7 @@ public partial class UserDetailViewModel : ViewModelBase
     {
         await base.LoadDataAsync();
 
-        User = await _userFacade.GetAsync(UserId);
+        Users = await _userFacade.GetAsync();
     }
 
 }
