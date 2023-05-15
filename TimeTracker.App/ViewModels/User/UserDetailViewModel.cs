026db2164cc90dca0fefd3cfb86b7d0c970c8e73
Marek Prochazka <m.prochazka2002@gmail.com>
2023-05-15 18:26:53 +0200

    update

--- TimeTracker.App/ViewModels/User/UserDetailViewModel.cs
+++ TimeTracker.App/ViewModels/User/UserDetailViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TimeTracker.App.Services;
+using TimeTracker.App.Services.Interfaces;
 using TimeTracker.BL.Facades;
 using TimeTracker.BL.Models;
 
@@ -12,30 +13,38 @@ namespace TimeTracker.App.ViewModels.User;
 public partial class UserDetailViewModel : ViewModelBase
 {
     private readonly IUserFacade _userFacade;
+    private readonly IActivityFacade _activityFacade;
     private readonly INavigationService _navigationService;
     private readonly IAlertService _alertService;
+    private readonly IActiveUserService _activeUserService;
 
 
-    public IEnumerable<UserListModel> Users { get; private set; }
+    public UserDetailModel? User { get; private set; }
+    public IEnumerable<ActivityListModel> UserActivities { get; set; } = null!;
 
     public UserDetailViewModel(
             IUserFacade userFacade,
+            IActivityFacade activityFacade,
             INavigationService navigationService,
+            IActiveUserService activeUserService,
             IMessengerService messengerService,
             IAlertService alertService
         )
         : base(messengerService)
     {
         _userFacade = userFacade;
+        _activityFacade = activityFacade;
         _navigationService = navigationService;
         _alertService = alertService;
+        _activeUserService = activeUserService;
     }
 
     protected override async Task LoadDataAsync()
     {
         await base.LoadDataAsync();
 
-        Users = await _userFacade.GetAsync();
+        User = await _userFacade.GetAsync(_activeUserService.GetId());
+        UserActivities = await _activityFacade.GetActivitiesByUserAssignedAsync(_activeUserService.GetId());
     }
 
 }
